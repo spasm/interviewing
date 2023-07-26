fca196b37e6b1e17a530b38c9fc267e2c861e4dd
Jeff S <jeffescott@gmail.com>
2023-07-25 17:28:26 -0700

    Update code.cs

--- code.cs
+++ code.cs
@@ -1,1 +1,70 @@
-// code goes here
+using System;
+using System.Collections.Generic;
+using ThirdPartySDK;
+
+public class ShoppingCart
+{
+    public static List<Product> Items = new List<Product>();
+
+    // From ThirdPartySDK
+    private InventoryService _inventoryService;
+
+    public ShoppingCart()
+    {
+        _inventoryService = new InventoryService(); 
+    }
+
+    public void AddBookToCart(Product book)
+    {
+
+        if (_inventoryService.CheckInventory(book.Name) > 0)
+        {
+            double discount = CalculateDiscount("Book", book.Price);
+            book.Price -= discount;
+            Items.Add(book);
+        }
+    }
+
+    public void AddCDToCart(Product cd)
+    {
+        if (_inventoryService.CheckInventory(cd.Name) > 0)
+        {
+            var discount = CalculateDiscount("CD", cd.Price);
+            cd.Price -= discount;
+            Items.Add(cd);
+        }
+    }
+
+    private double CalculateDiscount(string productType, double price, int quantity)
+    {
+        double discountRate = 0;
+
+        switch (productType)
+        {
+            case "Book":
+                discountRate = 0.1;
+                discountAfterQty = 2;
+                break;
+            case "CD":
+                discountRate = 0.15;
+                discountAfterQty = 5;
+                break;
+        }
+
+        return price * discountRate;
+    }
+}
+
+public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+    public int ItemType { get; set; }
+}
+
+public enum ItemType
+{
+    Digital,
+    Physical
+}
